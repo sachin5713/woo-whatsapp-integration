2b100e506f5cd9ecc43a0a3855242e0d43195427
Sachin <sachin.suthar2493@gmail.com>
2022-07-04 18:54:44 +0530

    modified main file

--- woo-whatsapp-integration.php
+++ woo-whatsapp-integration.php
@@ -1,5 +1,6 @@
 <?php
-/** Plugin Name: WooCommerce Whatsapp Notifications
+/** 
+* Plugin Name: WooCommerce Whatsapp Notifications
 * Version: 1.0.0
 * Plugin URI: #
 * Description: Sends Whatsapp notifications to your clients for order status changes. You can also receive an Whatsapp message when a new order is received.
@@ -21,7 +22,6 @@ if( !function_exists('wwn_activation') ) {
     register_activation_hook (__FILE__, 'wwn_activation');
     function wwn_activation() {   
         require plugin_dir_path( __FILE__ ).'includes/class-wwn-api-settings.php';
-        //require plugin_dir_path( __FILE__ ).'includes/class-wwn-display-country.php';
         require plugin_dir_path( __FILE__ ).'hooks.php';
         require plugin_dir_path( __FILE__ ).'admin/index.php'; 
     }
